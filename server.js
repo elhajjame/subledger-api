3238a4ff7e67b0e5df0a6da064fc4833f4af2410
mehdi el-hajjame <el-hajjame@outlook.com>
2026-03-09 15:20:59 +0000

    connecting database and server

---
+++ server.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: './config.env' });
+
+mongoose.connect(process.env.CONNECT_DB).then((con) => {
+  console.log('database connected successfully');
+})
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+  console.log(`the server running on the port ${port}`);
+})
+
+
