f17e6d55046f41a1b4d78078fecfa831cf52fa02
mehdi el-hajjame <el-hajjame@outlook.com>
2026-03-11 22:23:42 +0000

    implement user signin authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,25 @@
+
+import jwt from 'jsonwebtoken';
 import { User } from "../models/User.js";
 
 const signup = async (req, res) => {
   try {
-    const newUser = await User.create(req.body)
+    // const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRED_IN
+    })
 
     res.status(202).json({
       status: 'success',
       message: 'the user has been created successfully',
+      token,
       data: {
         user: newUser,
       }
