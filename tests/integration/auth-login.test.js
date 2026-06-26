855e9936e98c82cd9ef89085fe32e238bbfa6807
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-06-26 15:37:27 +0100

    test: add signup integration tests with Mocha

--- tests/integration/auth-login.test.js
+++ tests/integration/auth-login.test.js
@@ -1,36 +1,4 @@
-// import supertest from "supertest";
-// import app from "../../app";
 
-// describe("POST api/users/login", () => {
-//   describe("when given valid data", () => {
-//     test("respond with a 200 status code", async () => {
-//       const res = await supertest(app).post("/api/users/login").send({
-//         email: "test@example.com",
-//         password: "password123",
-//       });
-//       expect(res.statusCode).toBe(200);
-//       expect(res.headers["content-type"]).toMatch(/application\/json/);
-//     });
-//   });
-
-//   describe("when the email is messing", () => {
-//     test("response with a 404 status code", async () => {
-//       const res = await supertest(app).post("/api/users/login").send({
-//         password: "password123",
-//       });
-//       expect(res.statusCode).toBe(404);
-//     });
-//   });
-
-//   describe("when the password missing", () => {
-//     test("response with a status code 404", async () => {
-//       const res = await supertest(app).post("/api/users/login").send({
-//         email: "test@example.com",
-//       });
-//       expect(res.statusCode).toBe(404);
-//     });
-//   });
-// });
 import request from "supertest";
 import { expect } from "chai";
 import app from "../../app.js";
@@ -67,3 +35,47 @@ describe("POST /api/users/login", () => {
     });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+// import supertest from "supertest";
+// import app from "../../app";
+
+// describe("POST api/users/login", () => {
+//   describe("when given valid data", () => {
+//     test("respond with a 200 status code", async () => {
+//       const res = await supertest(app).post("/api/users/login").send({
+//         email: "test@example.com",
+//         password: "password123",
+//       });
+//       expect(res.statusCode).toBe(200);
+//       expect(res.headers["content-type"]).toMatch(/application\/json/);
+//     });
+//   });
+
+//   describe("when the email is messing", () => {
+//     test("response with a 404 status code", async () => {
+//       const res = await supertest(app).post("/api/users/login").send({
+//         password: "password123",
+//       });
+//       expect(res.statusCode).toBe(404);
+//     });
+//   });
+
+//   describe("when the password missing", () => {
+//     test("response with a status code 404", async () => {
+//       const res = await supertest(app).post("/api/users/login").send({
+//         email: "test@example.com",
+//       });
+//       expect(res.statusCode).toBe(404);
+//     });
+//   });
+// });
