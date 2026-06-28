a7f1921c64b279e88b94c4ac523acb79197c2daa
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-06-28 14:26:18 +0100

    ci: add GitHub Actions workflow

--- tests/setup.js
+++ tests/setup.js
@@ -1,24 +1,18 @@
-// import dotenv from "dotenv";
-// dotenv.config({ path: "./config.env" });
-
-// import mongoose from "mongoose";
-
-// beforeAll(async () => {
-//   await mongoose.connect("mongodb://localhost:27017/subledger");
-// });
-
-// afterAll(async () => {
-//   await mongoose.connection.close();
-// });
 import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
 
 import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer;
 
 before(async function () {
-  await mongoose.connect("mongodb://localhost:27017/subledger");
+  mongoServer = await MongoMemoryServer.create();
+
+  await mongoose.connect(mongoServer.getUri());
 });
 
 after(async function () {
   await mongoose.connection.close();
+  await mongoServer.stop();
 });
