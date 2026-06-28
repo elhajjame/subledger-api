9a106bf321a62ae60cba76fe4733e9a4bfa32230
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-06-28 14:11:52 +0100

    test: add integration tests for subscription creation

--- tests/integration/subscriptions.test.js
+++ tests/integration/subscriptions.test.js
@@ -1,3 +1,58 @@
+import request from "supertest";
+import { expect } from "chai";
+import app from "../../app.js";
+
+let token;
+
+before(async () => {
+  const loginRes = await request(app).post("/api/users/login").send({
+    email: "testor@example.com",
+    password: "testor1234",
+  });
+
+  token = loginRes.body.token;
+});
+
+describe("POST /api/subscriptions/createSub", () => {
+  describe("when given valid data", () => {
+    it("should respond with a 202 status code", async () => {
+      const res = await request(app)
+        .post("/api/subscriptions/createSub")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          name: "subscription test name",
+          price: 10,
+          billingCycle: "monthly",
+          createdAt: Date.now(),
+          userId: "6a3d6b243752e52ac48bdf02",
+        });
+
+      expect(res.status).to.equal(202);
+    });
+  });
+
+  describe("when the given information is incorrect", () => {
+    it("should respond with a 404 status code", async () => {
+      const subscriptionDataBody = [
+        { name: "wifi" },
+        { price: 100 },
+        { billingCycle: "monthly" },
+        { createdAt: Date.now() },
+      ];
+
+      for (const body of subscriptionDataBody) {
+        const res = await request(app)
+          .post("/api/subscriptions/createSub")
+          .set("Authorization", `Bearer ${token}`)
+          .send(body);
+
+        expect(res.status).to.equal(404);
+        expect(res.headers["content-type"]).to.match(/application\/json/);
+      }
+    });
+  });
+});
+
 // import supertest from "supertest";
 // import app from "../../app";
 
