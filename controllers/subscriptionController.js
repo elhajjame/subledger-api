f5b89150c10ab2850f5ab6d28484d1f16d1fa1b1
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-06-29 10:34:59 +0100

    fix: create test user before login in auth integration tests

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -7,20 +7,20 @@ const createSubscription = async (req, res) => {
       price: req.body.price,
       billingCycle: req.body.billingCycle,
       createdAt: req.body.createdAt,
-      userId: req.body.userId
+      userId: req.user._id,
     });
     res.status(202).json({
-      status: 'success',
-      message: 'the subscription has been created successfully',
+      status: "success",
+      message: "the subscription has been created successfully",
       data: {
-        NewSubscription
-      }
-    })
+        NewSubscription,
+      },
+    });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
-      message: err.message
-    })
+      status: "fail",
+      message: err.message,
+    });
   }
 };
 
@@ -29,17 +29,16 @@ const getAllSubscriptions = async (req, res) => {
     const subscriptions = await Subscription.find({ userId: req.user._id });
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       results: subscriptions.length,
       data: {
-        subscriptions
-      }
+        subscriptions,
+      },
     });
-
   } catch (err) {
     res.status(500).json({
-      status: 'fail',
-      message: err.message
+      status: "fail",
+      message: err.message,
     });
   }
 };
@@ -48,70 +47,79 @@ const getSubscription = async (req, res) => {
   try {
     const subscription = await Subscription.findOne({
       _id: req.params.id,
-      userId: req.user.id
+      userId: req.user.id,
     });
     console.log(subscription);
     if (!subscription) {
       return res.status(404).json({
-        status: 'fail',
-        message: 'Subscription not found'
+        status: "fail",
+        message: "Subscription not found",
       });
     }
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
-        subscription
-      }
+        subscription,
+      },
     });
-
   } catch (err) {
     res.status(500).json({
-      status: 'fail',
-      message: err.message
+      status: "fail",
+      message: err.message,
     });
   }
 };
 
 const updateSubscription = async (req, res) => {
   try {
-    const subscription = await Subscription.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const subscription = await Subscription.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
     if (!subscription) {
       return res.status(404).json({
-        status: 'fail',
-        message: 'the subscription dose not exist'
+        status: "fail",
+        message: "the subscription dose not exist",
       });
     }
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
-        subscription
-      }
-    })
+        subscription,
+      },
+    });
   } catch (error) {
     res.status(400).json({
-      status: 'fail',
-      message: 'can not update subscription information',
-      errorMessage: error.message
-    })
+      status: "fail",
+      message: "can not update subscription information",
+      errorMessage: error.message,
+    });
   }
-}
+};
 
 const deleteSubscription = async (req, res) => {
   try {
     const subscription = await Subscription.findByIdAndDelete(req.params.id);
     res.status(200).json({
-      status: 'success',
-      message: 'the subscription has been deleted successfully',
+      status: "success",
+      message: "the subscription has been deleted successfully",
     });
   } catch (err) {
     res.status(204).json({
-      status: 'success',
-      data: null
+      status: "success",
+      data: null,
     });
-  };
-}
-export { createSubscription, getAllSubscriptions, getSubscription, updateSubscription, deleteSubscription };
+  }
+};
+export {
+  createSubscription,
+  getAllSubscriptions,
+  getSubscription,
+  updateSubscription,
+  deleteSubscription,
+};
--- tests/integration/auth-singup.test.js
+++ tests/integration/auth-singup.test.js
@@ -1,29 +1,30 @@
-import request from "supertest";
-import { expect } from "chai";
-import app from "../../app.js";
+import supertest from "supertest";
+// import authController from "../../controllers/authController";
+import { request, response } from "express";
+import app from "../../app";
 
 describe("POST /api/users/signup", () => {
   describe("given valid signup information", () => {
-    it("should respond with a 202 status code", async () => {
-      const timestamp = Date.now();
-
-      const res = await request(app)
+    // should save data in database
+    // respond with a json obj and 200 status code
+    test("respond with a 200 status code", async () => {
+      const res = await supertest(app)
         .post("/api/users/signup")
         .send({
-          name: `mehdi-${timestamp}`,
-          email: `mehdi-${timestamp}@gmail.com`,
+          name: `mehdi-${Date.now()}`,
+          email: `mehdi-${Date.now()}@gmail.com`,
           role: "user",
-          password: `${timestamp}`,
-          passwordConfirm: `${timestamp}`,
+          password: `${Date.now()}`,
+          passwordConfirm: `${Date.now()}`,
         });
-
-      expect(res.status).to.equal(202);
-      expect(res.headers["content-type"]).to.match(/application\/json/);
+      expect(res.statusCode).toBe(202);
+      expect(res.headers["content-type"]).toMatch(/application\/json/);
     });
   });
 
-  describe("when the given information is incorrect", () => {
-    it("should respond with a 404 status code", async () => {
+  describe("when the given info are incorrect ", () => {
+    // respond with a statusCode 400
+    test("response whit statusCond 400", async () => {
       const bodyData = [
         { name: "name" },
         { email: "test@email.com" },
@@ -31,54 +32,10 @@ describe("POST /api/users/signup", () => {
         { password: "password1234" },
         { passwordConfirm: "password1234" },
       ];
-
       for (const body of bodyData) {
-        const res = await request(app).post("/api/users/signup").send(body);
-
-        expect(res.status).to.equal(404);
+        const res = await supertest(app).post("/api/users/signup").send(body);
+        expect(res.statusCode).toBe(404);
       }
     });
   });
 });
-
-// import supertest from "supertest";
-// // import authController from "../../controllers/authController";
-// import { request, response } from "express";
-// import app from "../../app";
-
-// describe("POST /api/users/signup", () => {
-//   describe("given valid signup information", () => {
-//     // should save data in database
-//     // respond with a json obj and 200 status code
-//     test("respond with a 200 status code", async () => {
-//       const res = await supertest(app)
-//         .post("/api/users/signup")
-//         .send({
-//           name: `mehdi-${Date.now()}`,
-//           email: `mehdi-${Date.now()}@gmail.com`,
-//           role: "user",
-//           password: `${Date.now()}`,
-//           passwordConfirm: `${Date.now()}`,
-//         });
-//       expect(res.statusCode).toBe(202);
-//       expect(res.headers["content-type"]).toMatch(/application\/json/);
-//     });
-//   });
-
-//   describe("when the given info are incorrect ", () => {
-//     // respond with a statusCode 400
-//     test("response whit statusCond 400", async () => {
-//       const bodyData = [
-//         { name: "name" },
-//         { email: "test@email.com" },
-//         { role: "user" },
-//         { password: "password1234" },
-//         { passwordConfirm: "password1234" },
-//       ];
-//       for (const body of bodyData) {
-//         const res = await supertest(app).post("/api/users/signup").send(body);
-//         expect(res.statusCode).toBe(404);
-//       }
-//     });
-//   });
-// });
